851d95144a0d6b17bb0448877ae8dd0c818ddd99
mats363 <81287846+mats363@users.noreply.github.com>
2021-12-04 17:59:30 +0100

    major update to functionality

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ let footer = document.createElement("footer");
 let userNameInput = document.createElement("input");
 let passWordInput = document.createElement("input");
 let btn = document.createElement("button");
+let btnLogOut = document.createElement("button");
 let login = document.createElement("div");
 let logo = document.createElement("h1");
 
+passWordInput.type = "password";
 
 userNameInput.placeholder = "Username";
 passWordInput.placeholder = "Password";
 btn.innerHTML = "Logga in";
+btnLogOut.innerHTML = "Logga ut";
 logo.innerHTML ="SampleDoctor";
 footer.innerHTML ="Copyright 2021 - SampleDoctor.com";
 
 root.append(menu, section, footer);
-menu.append(login, logo);
-login.append(userNameInput, passWordInput, btn); 
-
 
 window.addEventListener("load", initPage);
 // Array with user objects
@@ -46,7 +46,17 @@ let users = [
 
 // Adds user array to Localstorage on load  
 function initPage() {
+    menu.append(login, logo);
+    login.append(userNameInput, passWordInput, btn); 
+    section.innerHTML = "Logga in";
     localStorage.setItem("Users", JSON.stringify(users));
+    let isLoggedIn = localStorage.getItem("loginStatus");
+    if (isLoggedIn == "True") {
+        console.log("Inloggad");
+        renderLoggedInPage();
+    } else {
+        renderFailedToLogIn();
+    }
 }
 function getUsers() {
     // Hämta array från Localstorage - kolla om inlogget stämmer
@@ -59,35 +69,68 @@ function getUsers() {
     return userList;
 }
 
-function logIn() {
-    let userList = getUsers();
-    console.log(userList);
-}
 
-function renderPage() {
+
+function renderLoggedInPage() {
+    
+    section.innerHTML = "Grattis du är inloggad";
+    login.innerHTML = "Välkommen " + userNameInput.value;
+    localStorage.setItem("loginStatus", "True");
+    login.append(btnLogOut);
+    
+
     // Renderar en inloggad vy (både header och section) med information om samplingar
 }
 
-function renderError() {
+function renderFailedToLogIn() {
     // Renderar "felaktigt användarnamn"
+    login.append(userNameInput, passWordInput, btn);
+    section.innerHTML = "Du har angett fel användarnamn eller lösenord";
+    localStorage.setItem("loginStatus", "False");
+}
+
+function renderLoggedOutPage() {
+    section.innerHTML = "Du är utloggad"
+    login.innerHTML = "";
+    login.append(userNameInput, passWordInput, btn);
+    
+    
 }
 
 // Vid klick på knappen körs funktionen logIn
 btn.addEventListener("click", checkLogin);
 
 function checkLogin() {
-    //let userName = userNameInput.value;
-    //let passWord = passWordInput.value;
-    let newUser = [ {
-        name: userNameInput.value,
-        passWord: passWordInput.value
-    }]
-
-    console.log(newUser);
-
+    userName = userNameInput.value;
+    passWord = passWordInput.value;
+    
     let userList = getUsers();
 
-    
-    
+    for (i = 0; i < userList.length; i++) {
+        if (userList[i].name == userName && userList[i].password == passWord) {
+        renderLoggedInPage();
+        console.log("Inloggad");
+        //localStorage.setItem("Inloggad", "Ja")
+        break;
+    } else {
+        renderFailedToLogIn();
+        console.log("Inte inloggad");
+        //localStorage.setItem("Inloggad", "Nej")
+        break;
+    }
+
+    }
 }
 
+btnLogOut.addEventListener("click", logOut);    
+userNameInput.clear();
+
+function logOut() {
+    //document.getElementsByName("input")
+    userNameInput.value = "";
+    passWordInput.value = "";
+    localStorage.getItem("loginStatus");
+    localStorage.setItem("loginStatus", "False");
+    renderLoggedOutPage();
+
+}
